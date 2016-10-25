cd7e8c76a4604de17b4f19950ef671305cd9e6e4
Brian Hedberg <bshslowfood@gmail.com>
2016-10-25 15:58:31 -0500

    enable debugging, work without merging extensions

--- _plugins/redcarpet.rb
+++ _plugins/redcarpet.rb
@@ -2,38 +2,41 @@
 #require 'jekyll'
 require 'redcarpet'
 require 'rexml/document'
-module Jekyll
-class RedcarpetExtender < Redcarpet::Render::HTML
-  # Extend the table to be compatible with the Bootstrap css.
-#   def header(title, level)
-  
-#      @headers ||= []
+require 'pry'
+require 'rb-readline'
 
-#       title_elements = REXML::Document.new(title)
-#       flattened_title = title_elements.inject('') do |flattened, element|
-#         flattened +=  if element.respond_to?(:text)
-#                         element.text
-#                       else
-#                         element.to_s
-#                       end
-#       end
-#       id_name = flattened_title.downcase.gsub(/[^a-z\s]/, '').gsub(/\W+/, "-")
-      
-#       #TODO : implement this as its own method
-#       if @headers.include?(id_name)
-#         id_name += "_1"
-        
-#         loop do
-#           break if !@headers.include?(id_name)
-#           # generate titles like foo-bar_1, foo-bar_2
-#           id_name.gsub!(/\_(\d+)$/, "_#{$1.to_i + 1}")
-#         end
-#       end
-#       @headers << id_name
-    
-#       #<h2 id="someheader">someheader</h2>
-#       %(\n<h#{level} id="#{id_name}">#{title}</h#{level}>\n)
-#     end
+module Redcarpet
+  module Render
+    class RedcarpetExtender < HTML
+  # Extend the table to be compatible with the Bootstrap css.
+  def header(title, level)
+
+
+     # @headers ||= []
+
+  #     title_elements = REXML::Document.new(title)
+  #     flattened_title = title_elements.inject('') do |flattened, element|
+  #       flattened +=  if element.respond_to?(:text)
+  #                       element.text
+  #                     else
+  #                       element.to_s
+  #                     end
+  #     end
+      id_name = title.downcase.gsub(/[^a-z\s]/, '').gsub(/\W+/, "-")
+
+  #     #TODO : implement this as its own method
+  #     if @headers.include?(id_name)
+  #       id_name += "_1"
+
+  #       loop do
+  #         break if !@headers.include?(id_name)
+  #         # generate titles like foo-bar_1, foo-bar_2
+  #         id_name.gsub!(/\_(\d+)$/, "_#{$1.to_i + 1}")
+  #       end
+  #     end
+  #     @headers << id_name
+      %(\n<h#{level} id="#{id_name}">#{title}</h#{level}>\n)
+    end
 end
 
 class Jekyll::Converters::Markdown::RedcarpetExt
@@ -44,15 +47,16 @@ class Jekyll::Converters::Markdown::RedcarpetExt
   def extensions
     Hash[ *@site_config['redcarpet']['extensions'].map {|e| [e.to_sym, true]}.flatten ]
   end
-  
+
   def markdown
-    puts extensions
     @markdown ||= Redcarpet::Markdown.new(RedcarpetExtender, extensions)
   end
 
   def convert(content)
     return super unless @site_config['markdown'] == 'RedcarpetExt'
     markdown.render(content)
+
   end
+end
 end
 end
